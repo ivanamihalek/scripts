418783d21acd30f26326306b16a71d8239cb7ee8
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-17 02:40:46 +0100

    some more on modeller

--- modeller/loop.py
+++ modeller/loop.py
@@ -11,12 +11,12 @@ def main():
     
     if len(sys.argv)<6:
         print "usage: %s <pdb>> <loopfile>  <chain> <from> <to>" % sys.argv[0]
-        print "\t pdb and loopfile sould be the output of build_dummy.pl\n"
-        print "\t from and to should be the first two numbers in loopfile, \n"
-        print "\t unless we are prepending/appending a sequence \n"
-        print "\t in which case the numbers should be correcte for 3 res on the nonexiting side\n"
-        print "\t Whe prepending/appending the corresponding chain \n"
-        print "\t should be indicated by '-' on the command line\n"
+        print "\t pdb and loopfile sould be the output of build_dummy.pl"
+        print "\t from and to should be the first two numbers in loopfile,"
+        print "\t unless we are prepending/appending a sequence"
+        print "\t in which case the numbers should be correcte for 3 res on the nonexiting side"
+        print "\t Whe prepending/appending the corresponding chain"
+        print "\t should be indicated by '-' on the command line"
         exit(1)
 
     [pdbfile, seqfile, chain, chain_from, chain_to] = sys.argv[1:6]
