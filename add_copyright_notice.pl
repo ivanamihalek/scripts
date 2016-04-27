92150e9c6965f0cb2dcaad5ee8d6a20f84a1f4ef
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-04-27 18:34:18 -0400

    copyright notice

---
+++ add_copyright_notice.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl -w
+
+@ARGV>=3 ||
+    die "Usage:  $0  <file name> <after line no> <copyrigt text file> \n";
+
+($filename,  $lineno, $cptxt) = @ARGV;
+
+open (IF, "<$filename" ) 
+    || die "Cno $filename: $!.\n";
+
+open (OF, ">tmp_tmp") ||
+    die  "Cno tmp_tmp: $!.\n";
+
+$line_ct = 0;
+while ( <IF> ) {
+    print OF;
+    if ($line_ct == $lineno) {
+	$ret =   `cat $cptxt`;
+	print OF $ret;
+	print OF;
+    }
+    $line_ct ++;
+}
+close OF;
+close IF;
+
+`mv tmp_tmp  $filename`;
