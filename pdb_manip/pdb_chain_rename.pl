86c2320ca3f394c9c2b0dac3fc38602f8101241d
ivana <ivana.mihalek@gmail.com>
2017-06-24 13:46:37 -0400

    pdb chain rename compactified

--- pdb_manip/pdb_chain_rename.pl
+++ pdb_manip/pdb_chain_rename.pl
@@ -1,4 +1,6 @@
 #! /usr/bin/perl -w
+use strict;
+use warnings;
 use IO::Handle;         #autoflush
 # FH -> autoflush(1);
 
@@ -6,57 +8,35 @@ defined ( $ARGV[0]  ) ||
     die "Usage: $0   <pdb_file> ".
     "  [<old_chain_name> <new_chain_name> <res from> <res to>].\n";
 
-$pdbfile = $ARGV[0];
-if ( defined $ARGV[1] ) {
-    $old_name = $ARGV[1];
-} else {
-    $old_name = "";
-}
-if ( defined $ARGV[2] ) {
-    $new_name = $ARGV[2];
-} else {
-    $new_name = "A";
-}
-if ( defined $ARGV[3] ) {
-    $from = $ARGV[3] ;
-} else {
-    $from = -100 ;
-}
-if ( defined $ARGV[4] ) {
-    $to = $ARGV[4] ;
-} else {
-    $to = 10000 ;
-}
+my $pdbfile = $ARGV[0];
+my $old_name = defined $ARGV[1]? $ARGV[1]:"";
+my $new_name = defined $ARGV[2]? $ARGV[2]:"A";
+my $from     = defined $ARGV[3]? $ARGV[3]:-100;
+my $to       = defined $ARGV[4]? $ARGV[4]:-100000;
 
 open ( IF, "<$pdbfile") ||
     die "Cno $pdbfile: $!.\n";
 
-
-    
 while ( <IF> ) {
 
     if ( ! /^ATOM/ && ! /^HETATM/ ) {
-	print  ;
-	next;
+        print  ;
+        next;
     }
-
-    $res_seq  = substr $_, 22, 4;  $res_seq=~ s/\s//g;
-    $chain_name = substr ( $_,  21, 1);
+    my $res_seq  = substr $_, 22, 4;  $res_seq=~ s/\s//g;
+    my $chain_name = substr ( $_,  21, 1);
 
     if ($old_name && $chain_name ne $old_name) {
-	print;
-	next;
+        print;
+        next;
     }
-    
     if ( $res_seq < $from  ||  $res_seq > $to) {
-	print;
-	next;
+        print;
+        next;
     }
-
-    $line = $_;
+    my $line = $_;
     substr ( $line,  21, 1) = $new_name;
     print $line;
-
 }
 
 close IF;
