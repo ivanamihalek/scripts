9ce30b74d7cbc7468b0d09f9e9fddbec33837efe
Ivana Mihalek <ivana.mihalek@gmail.com>
2019-05-13 14:26:34 +0200

    estract resnums

--- pdb_manip/pdb_extract_res_numbers.pl
+++ pdb_manip/pdb_extract_res_numbers.pl
@@ -1,18 +1,22 @@
 #! /usr/bin/perl -w
 
-@ARGV ||
-    die "Usage:  $0  <file name> \n";
+@ARGV>1 || die "Usage:  $0  <file name>  <chain_name> \n";
 
 $filename = $ARGV[0];
-open (IF, "<$filename" ) 
+$target_chain = $ARGV[1];
+
+open (IF, "<$filename" )
     || die "Cno $filename: $!.\n";
 
-while ( <> ) {
+while ( <IF> ) {
     /^ATOM/ || next;
+    $chain_name = substr $_, 21, 1;
+    $chain_name eq $target_chain || next;
     $atom_name = substr $_, 12, 4; $atom_name=~ s/\s//g;
     $atom_name eq 'N' || next;
     $res_seq   = substr $_, 22, 5;  $res_seq=~ s/\s//g;
-    print $res_seq, "\n";
+    $res_name = substr $_, 17, 3;  $res_name=~ s/\s//g;
+    print "$res_seq\t$res_name\n";
 }
 
 close IF;
